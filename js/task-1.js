0ad91a76c7b5315db2e4c6b24199917ada573260
OlexanderMotylgit config --global user.name OlexanderMotyl[200~git config --global user.name your_name00~git config --global user.name your_name ~git config --global user.name your_namegit config --global user.name your_namegit config --global user.name OlexanderMotylgit config --global user.email JustSanya1@gmail.com <JustSanya1@gmail.com>
2025-06-11 22:35:24 +0200

    script

--- js/task-1.js
+++ js/task-1.js
@@ -4,6 +4,14 @@ function makeTransaction(quantity, pricePerDroid) {
 };
 
 
+function checkAge(age) {
+    if (age >= 18) {
+       return console.log(`You are an adult`);
+    }
+
+   }
+   
+
 
 
 
@@ -12,3 +20,5 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(checkAge(20));
